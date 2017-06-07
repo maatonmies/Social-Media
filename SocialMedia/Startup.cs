c273110ad47e4fed25989ce21c65f25542ccf329
Maaton Mies <kisvardaymarton@icloud.com>
2017-06-07 13:48:57 +0100

    Project Setup

---
+++ SocialMedia/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SocialMedia.Startup))]
+
+namespace SocialMedia
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
